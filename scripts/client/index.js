bf353c10aaab433492e577994e61c112602852b1
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-10-15 17:06:28 -0500

    Re-add index files for RAGEMP

---
+++ scripts/client/index.js
@@ -0,0 +1,34 @@
+// For RAGEMP only
+
+// Shared Scripts
+require("shared/const.js");
+require("shared/utilities.js");
+require("shared/gamedata.js");
+
+// Multiplayer Mod (Wrapped Natives)
+require("native/ragemp.js");
+
+// Client Scripts
+require("gui.js");
+require("main.js");
+require("nametag.js");
+require("sync.js");
+require("scoreboard.js");
+require("keybind.js");
+require("chatbox.js");
+require("label.js");
+require("skin-select.js");
+require("server.js");
+require("job.js");
+require("event.js");
+require("item.js");
+require("utilities.js");
+require("messaging.js");
+require("logo.js");
+require("afk.js");
+require("mousecam.js");
+require("radio.js");
+require("animation.js");
+
+// Startup
+require("startup.js");
---
+++ scripts/server/index.js
@@ -0,0 +1,86 @@
+// Shared Scripts
+require("shared/const.js");
+require("shared/utilities.js");
+require("shared/gamedata.js");
+
+// Multiplayer Mod (Wrapped Natives)
+require("native/ragemp.js");
+
+// Server Scripts
+require("class.js");
+require("accent.js");
+require("account.js");
+require("animation.js");
+require("anticheat.js");
+require("ban.js");
+require("bitflag.js");
+require("business.js");
+require("chat.js");
+require("clan.js");
+require("client.js");
+require("colour.js");
+require("const.js");
+require("database.js");
+require("developer.js");
+require("discord.js");
+require("economy.js");
+require("email.js");
+require("event.js");
+require("fishing.js");
+require("gui.js");
+require("help.js");
+require("house.js");
+require("item.js");
+require("job.js");
+require("keybind.js");
+require("locale.js");
+require("messaging.js");
+require("misc.js");
+require("npc.js");
+require("staff.js");
+require("race.js");
+require("radio.js");
+require("security.js");
+require("subaccount.js");
+require("timers.js");
+require("trigger.js");
+require("utilities.js");
+require("vehicle.js");
+require("config.js");
+require("core.js");
+require("command.js");
+
+// Server Business Scripts
+require("business/bakery.js");
+require("business/bar.js");
+require("business/burger.js");
+require("business/clothing.js");
+require("business/club.js");
+require("business/fuel.js");
+require("business/mechanic.js");
+require("business/pizza.js");
+require("business/restaurant.js");
+require("business/vehicle.js");
+require("business/weapon.js");
+
+// Server Job Scripts
+require("job/bus.js");
+require("job/drug.js");
+require("job/fire.js");
+require("job/garbage.js");
+require("job/medic.js");
+require("job/police.js");
+require("job/taxi.js");
+require("job/weapon.js");
+
+// Server Item Scripts
+require("item/food.js");
+require("item/drink.js");
+require("item/walkie-talkie.js");
+require("item/phone.js");
+require("item/handcuff.js");
+require("item/rope.js");
+require("item/tazer.js");
+
+// Startup
+require("startup.js");
